20056ce8de800b875306aa637e6e1ebed62bf701
Mosejoro <monehinseun4125@gmail.com>
2024-10-18 12:42:43 +0100

    Moobile, Header, Subpage

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
   alert("Thank you for your message! We will get back to you soon.");
   this.reset();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var teamCarousel = new bootstrap.Carousel(
+    document.getElementById("teamCarousel", "teamCarousels"),
+    {
+      interval: 3000,
+      wrap: true,
+      keyboard: false,
+    }
+  );
+});
